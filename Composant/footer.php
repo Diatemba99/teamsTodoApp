b112aa936597a595e90943aa6630d96bf2f7f93a
EK_00001PE_2023 <martin.diatta99@egmail.com>
2023-09-19 14:23:44 +0000

    changing footer content

--- Composant/footer.php
+++ Composant/footer.php
@@ -8,7 +8,7 @@
 		<a href="javaScript:;" class="back-to-top"><i class='bx bxs-up-arrow-alt'></i></a>
 		<!--End Back To Top Button-->
 		<footer class="page-footer">
-			<p class="mb-0">Carantech © 2021. All right reserved.</p>
+			<p class="mb-0">Propulsed with ❤ by Inros Lackner All Rights Reserved. .</p>
 		</footer>
 	</div>
 	<!--end wrapper-->
